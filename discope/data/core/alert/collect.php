21ba378dc97f06f8d4d9ebd27b7dfb0c79881ad0
Lucas Laurent <lucas@yesbabylon.com>
2024-10-11 08:51:06 +0200

    Move documents package in discope

---
+++ discope/data/core/alert/collect.php
@@ -0,0 +1,59 @@
+<?php
+/*
+    This file is part of the Discope property management software.
+    Author: Yesbabylon SRL, 2020-2024
+    License: GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+use equal\orm\Domain;
+
+[$params, $providers] = eQual::announce([
+    'description'   => "Advanced search for Messages: returns a collection of Message according to extra parameters.",
+    'extends'       => 'core_model_collect',
+    'params'        => [
+        'entity' =>  [
+            'type'              => 'string',
+            'description'       => "Full name (including namespace) of the class to look into (e.g. \'core\\User\').",
+            'default'           => 'discope\core\alert\Message'
+        ],
+        'center_office_id' => [
+            'type'              => 'many2one',
+            'foreign_object'    => 'lodging\identity\CenterOffice',
+            'description'       => "Office the message relates to (for targeting the users)."
+        ],
+        'booking_id' => [
+            'type'              => 'many2one',
+            'foreign_object'    => 'lodging\sale\booking\Booking',
+            'description'       => "Booking the invoice relates to."
+        ]
+    ],
+    'response'      => [
+        'content-type'  => 'application/json',
+        'charset'       => 'utf-8',
+        'accept-origin' => '*'
+    ],
+    'providers'     => ['context']
+]);
+
+/**
+ * @var \equal\php\Context $context
+ */
+['context' => $context] = $providers;
+
+$domain = $params['domain'];
+
+if(isset($params['center_office_id']) && $params['center_office_id'] > 0) {
+    $domain = Domain::conditionAdd($domain, ['center_office_id', '=', $params['center_office_id']]);
+}
+
+if(isset($params['booking_id']) && $params['booking_id'] > 0) {
+    $domain = Domain::conditionAdd($domain, ['object_id', '=', $params['booking_id']]);
+}
+
+$params['domain'] = $domain;
+
+$result = eQual::run('get', 'model_collect', $params, true);
+
+$context->httpResponse()
+        ->body($result)
+        ->send();
---
+++ documents/classes/Export.class.php
@@ -0,0 +1,64 @@
+<?php
+/*
+    This file is part of the Discope property management software.
+    Author: Yesbabylon SRL, 2020-2024
+    License: GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+namespace documents;
+
+class Export extends Document {
+
+    public function getTable() {
+        return 'lodging_documents_export';
+    }
+
+    public static function getColumns() {
+        return [
+
+            'name' => [
+                'type'              => 'computed',
+                'result_type'       => 'string',
+                'function'          => 'calcName',
+                'store'             => true
+            ],
+
+            'center_office_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'lodging\identity\CenterOffice',
+                'description'       => 'Office the invoice relates to (for center management).',
+            ],
+
+            'export_type' => [
+                'type'              => 'string',
+                'selection'         => [
+                    'invoices',
+                    'payments'
+                ],
+                'required'          => true,
+                'readonly'          => true
+            ],
+
+            'is_exported' => [
+                'type'              => 'boolean',
+                'default'           => false,
+                'description'       => 'Mark the archive as already exported.'
+            ]
+
+        ];
+    }
+
+    public static function calcName($self) {
+        $result = [];
+        $self->read(['created', 'export_type', 'center_office_id' => ['name']]);
+
+        foreach($self as $id => $export) {
+            $result[$id] = sprintf('%s - %s - %s',
+                date('Ymd', $export['created']),
+                $export['export_type'],
+                $export['center_office_id']['name']
+            );
+        }
+
+        return $result;
+    }
+}
