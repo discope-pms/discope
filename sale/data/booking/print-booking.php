2e1d5adcb00858608fbca46d79c715697a415981
alexandraYesbabylon <alexandra@yesbabylon.com>
2025-04-18 10:44:13 +0000

    Upudate  the Print Booking Format Member function

--- sale/data/booking/print-booking.php
+++ sale/data/booking/print-booking.php
@@ -70,9 +70,15 @@ list($context, $orm) = [$providers['context'], $providers['orm']];
  * Methods
  */
 
-$lodgingBookingPrintBookingFormatMember = function($booking) {
-    $id = $booking['customer_id']['partner_identity_id']['id'];
-    $code = ltrim(sprintf("%3d.%03d.%03d", intval($id) / 1000000, (intval($id) / 1000) % 1000, intval($id)% 1000), '0');
+ $lodgingBookingPrintBookingFormatMember = function($booking) {
+    $has_number_assignment = Setting::get_value('sale', 'features', 'customer.member.has_number_assignment', 0);
+    if ($has_number_assignment) {
+        $customer_assignment = Setting::get_value('sale', 'features', 'customer.member.number_assignment');
+        $code = $booking['customer_id']['partner_identity_id'][$customer_assignment];
+    }else {
+        $id = $booking['customer_id']['partner_identity_id']['id'];
+        $code = ltrim(sprintf("%3d.%03d.%03d", intval($id) / 1000000, (intval($id) / 1000) % 1000, intval($id)% 1000), '0');
+    }
     return $code.' - '.$booking['customer_id']['partner_identity_id']['display_name'];
 };
 
@@ -130,6 +136,7 @@ else {
             'partner_identity_id' => [
                 'id',
                 'display_name',
+                'accounting_account',
                 'type',
                 'address_street', 'address_dispatch', 'address_city', 'address_zip', 'address_country',
                 'type',
