61c6d8fa2f224a89c64af6ebd106a5cb363ed6f3
Lucas <lucas@yesbabylon.com>
2026-01-09 13:28:45 +0100

    add swipe left and right on calendar

--- lathus/apps/employees/src/app/in/planning-employees/_components/planning-employees-calendar/planning-employees-calendar.component.ts
+++ lathus/apps/employees/src/app/in/planning-employees/_components/planning-employees-calendar/planning-employees-calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AppService } from '../../../../_services/app.service';
 import { ActivityMap } from '../../../../../type';
 
@@ -11,6 +11,51 @@ export class PlanningEmployeesCalendarComponent implements OnInit {
 
     public activityMap: ActivityMap = {};
 
+    private startX = 0;
+    private currentX = 0;
+    private isSwiping = false;
+    private swipeThreshold = 100;
+
+    @HostListener('touchstart', ['$event'])
+    onTouchStart(event: TouchEvent) {
+        event.preventDefault();
+        this.startX = event.touches[0].clientX;
+        this.isSwiping = true;
+    }
+
+    @HostListener('touchmove', ['$event'])
+    onTouchMove(event: TouchEvent) {
+        if (!this.isSwiping) return;
+        event.preventDefault();
+        this.currentX = event.touches[0].clientX;
+        let deltaX = this.currentX - this.startX;
+        if(deltaX > this.swipeThreshold) {
+            deltaX = this.swipeThreshold;
+        }
+        else if(deltaX < -this.swipeThreshold) {
+            deltaX = -this.swipeThreshold;
+        }
+        (event.target as HTMLElement).style.transform = `translateX(${deltaX}px)`;
+    }
+
+    @HostListener('touchend', ['$event'])
+    onTouchEnd(event: TouchEvent) {
+        if (!this.isSwiping) return;
+        this.isSwiping = false;
+
+        const deltaX = this.currentX - this.startX;
+        const host = event.target as HTMLElement;
+
+        if (deltaX < -this.swipeThreshold) {
+            this.onNextDate();
+        }
+        else if (deltaX > this.swipeThreshold) {
+            this.onPreviousDate();
+        }
+
+        host.style.transform = `translateX(0px)`;
+    }
+
     constructor(
         private app: AppService
     ) {
@@ -20,5 +65,15 @@ export class PlanningEmployeesCalendarComponent implements OnInit {
         this.app.activityMap$.subscribe((activityMap) => {
             this.activityMap = activityMap;
         });
+    }
+
+    private onPreviousDate() {
+        console.log('onPreviousDate');
+        this.app.setPreviousDate();
+    }
+
+    private onNextDate() {
+        console.log('onNextDate');
+        this.app.setNextDate();
     }
 }
