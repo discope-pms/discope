b9196681fe8b16b50e149a15c8c9f0e0b41476ed
Lucas <lucas@yesbabylon.com>
2026-01-09 10:25:29 +0100

    Add actions to auto activate and deactivate employees based on contracts dates

---
+++ hr/actions/employee/activate-employees.php
@@ -0,0 +1,50 @@
+<?php
+/*
+    This file is part of the Discope property management software <https://github.com/discope-pms/discope>
+    Some Rights Reserved, Discope PMS, 2020-2026
+    Original author(s): Yesbabylon SRL
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+use hr\employee\Employee;
+
+[$params, $providers] = eQual::announce([
+    'description'   => "Activates employees that have a pending contract.",
+    'help'          => "Can be used as a recurring task, triggered at the start of every day.",
+    'params'        => [
+    ],
+    'access'        => [
+        'visibility'    => 'private',
+    ],
+    'response'      => [
+        'content-type'  => 'application/json',
+        'charset'       => 'utf-8',
+        'accept-origin' => '*'
+    ],
+    'providers'     => ['context', 'orm']
+]);
+
+/**
+ * @var \equal\php\Context          $context
+ * @var \equal\orm\ObjectManager    $orm
+ */
+['context' => $context, 'orm' => $orm] = $providers;
+
+$employees = Employee::search([
+    [
+        ['is_active', '=', false],
+        ['date_start', '<=', time()],
+        ['date_end', 'is', null]
+    ],
+    [
+        ['is_active', '=', false],
+        ['date_start', '<=', time()],
+        ['date_end', '>=', time()]
+    ]
+])
+    ->update(['is_active' => true]);
+
+
+$context->httpResponse()
+        ->status(200)
+        ->send();
