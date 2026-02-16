e8c12819ec13ad098a626932abb32a3f2d9b3921
Lucas <lucas@yesbabylon.com>
2026-02-16 14:50:17 +0100

    add style to activities cards

--- lathus/apps/employees/src/app/in/planning-employees/_components/planning-employees-calendar/planning-employees-calendar.component.ts
+++ lathus/apps/employees/src/app/in/planning-employees/_components/planning-employees-calendar/planning-employees-calendar.component.ts
@@ -100,4 +100,25 @@ export class PlanningEmployeesCalendarComponent implements OnInit {
     private onNextDate() {
         this.app.setNextDate();
     }
+
+    public getActivityColor(activity: any): string {
+        if(activity.is_partner_event) {
+            const mapPartnerEventColors: any = {
+                camp_activity: '#7A8F78',
+                leave: '#BFA58A',
+                time_off: '#8C6E5E',
+                other: '#6C7A91',
+                rest: '#6F5B4D',
+                trainer: '#C27A5A',
+                training: '#8F4E3A'
+            };
+
+            return mapPartnerEventColors[activity.event_type];
+        }
+        else if(activity.product_model_id.activity_color) {
+            return activity.product_model_id.activity_color;
+        }
+
+        return '#BAA9A2';
+    }
 }
