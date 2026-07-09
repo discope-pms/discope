8f57a40a9918b28a58ec6330c9183ec25b09282b
Lucas <lucas@yesbabylon.com>
2026-07-09 14:10:46 +0200

    add discope center settings

---
+++ discope/classes/setting/Setting.class.php
@@ -0,0 +1,47 @@
+<?php
+/*
+    Developed by Yesbabylon - https://yesbabylon.com
+    (c) 2025-2026 Yesbabylon SA
+    Licensed under the GNU AGPL v3 License - https://www.gnu.org/licenses/agpl-3.0.html
+*/
+
+namespace discope\setting;
+
+class Setting extends \core\setting\Setting {
+
+    public static function getColumns() {
+        return [
+
+            'setting_values_ids' => [
+                'type'              => 'one2many',
+                'foreign_object'    => 'fmt\setting\SettingValue',
+                'foreign_field'     => 'setting_id',
+                'sort'              => 'asc',
+                'order'             => 'name',
+                'description'       => 'List of values related to the setting.'
+            ],
+
+            'setting_sequences_ids' => [
+                'type'              => 'one2many',
+                'foreign_object'    => 'fmt\setting\SettingSequence',
+                'foreign_field'     => 'setting_id',
+                'sort'              => 'asc',
+                'order'             => 'name',
+                'description'       => 'List of sequences related to the setting.'
+            ]
+
+        ];
+    }
+
+    protected static function getSelectorKeys() {
+        return ['user_id', 'organisation_id', 'center_office_id', 'center_id'];
+    }
+
+    protected static function getSettingValueClass(): string {
+        return SettingValue::class;
+    }
+
+    protected static function getSettingSequenceClass(): string {
+        return SettingSequence::class;
+    }
+}
---
+++ discope/classes/setting/SettingValue.class.php
@@ -0,0 +1,59 @@
+<?php
+/*
+    Developed by Yesbabylon - https://yesbabylon.com
+    (c) 2025-2026 Yesbabylon SA
+    Licensed under the GNU AGPL v3 License - https://www.gnu.org/licenses/agpl-3.0.html
+*/
+
+namespace discope\setting;
+
+class SettingValue extends \core\setting\SettingValue {
+
+    public static function getColumns() {
+        return [
+
+            'setting_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'fmt\setting\Setting',
+                'description'       => 'Setting the value relates to.',
+                'ondelete'          => 'cascade',
+                'required'          => true
+            ],
+
+            'user_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'identity\User',
+                'description'       => 'User the setting is specific to (optional).',
+                'ondelete'          => 'cascade'
+            ],
+
+            'organisation_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'identity\Organisation',
+                'description'       => 'Organisation the setting is specific to (optional).',
+                'ondelete'          => 'cascade'
+            ],
+
+            'center_office_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'identity\CenterOffice',
+                'description'       => 'Center office the setting is specific to (optional).',
+                'ondelete'          => 'cascade'
+            ],
+
+            'center_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'identity\Center',
+                'description'       => "Center the setting is specific to (optional).",
+                'ondelete'          => 'cascade'
+            ]
+
+        ];
+    }
+
+    public function getUnique() {
+        return [
+            ['setting_id', 'user_id', 'organisation_id', 'center_office_id', 'center_id']
+        ];
+    }
+}
