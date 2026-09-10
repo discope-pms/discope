a557b6aecc6135d4301e85f83b9a4a13feae2430
Lucas <lucas@yesbabylon.com>
2026-09-10 15:09:20 +0200

    send request for guest list encoding by customer

--- sale/actions/booking/send-contract.php
+++ sale/actions/booking/send-contract.php
@@ -123,9 +123,12 @@ list($context, $orm, $cron, $dispatch) = [$providers['context'], $providers['orm
 
 $booking = Booking::id($params['booking_id'])
     ->read([
-        'center_id' => ['id', 'center_office_id' => ['email_bcc']],
+        'is_from_channelmanager',
         'has_contract',
-        'contracts_ids'
+        'contracts_ids',
+        'center_id' => [
+            'center_office_id' => ['email_bcc']
+        ],
     ])
     ->first(true);
 
@@ -295,12 +298,9 @@ Mail::queue($message, 'sale\booking\Booking', $params['booking_id']);
     Schedule invite to fill in Guests List
 */
 
-
-/*
-// #todo - activate guest list invite
 // #memo - this should only be sent to specific bookings (GG / GA 'groups' / ???)
-if ($email !== null  && $lang !== null ) {
-    // schedule a task in 10 minutes to send the guest list
+if(!$booking['is_from_channelmanager']) {
+    // schedule a task in 10 minutes to send the guest list encoding invitation
     $cron->schedule(
         "booking.guest.email.send.invite.{$params['booking_id']}",
         time() + 600,
@@ -312,10 +312,6 @@ if ($email !== null  && $lang !== null ) {
         ]
     );
 }
-
-// #todo - also schedule a guest_list_check-sent (same way than contract)
-
-*/
 
 
 /*
